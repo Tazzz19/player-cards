e34ee40d565c333b9c42adde6d481a784078a1b4
Tazzz19 <hamzatrahman92@gmail.com>
2024-02-23 11:39:15 +0100

    Done

---
+++ player-cards/src/Players.jsx
@@ -0,0 +1,79 @@
+
+
+const players = [
+    {
+        name: 'Lionel Messi',
+        team: 'Bacernola',
+        nationality: 'Argentina',
+        jerseyNumber: 30,
+        age: 36,
+        imageUrl: './Images/Messi.webp',
+    },
+    {
+        name: 'Kevin De Bruyne',
+        team: 'Manchester City',
+        nationality: 'Belgian',
+        jerseyNumber: 17,
+        age: 31,
+        imageUrl: './Images/kdb.jpeg',
+    },
+    {
+        name: 'Cristiano Ronaldo',
+        team: 'Al Nassar',
+        nationality: 'Portugeese',
+        jerseyNumber: 7,
+        age: 37,
+        imageUrl: './Images/Ronaldo.jpg',
+    },
+    {
+        name: 'Jude Bellingham',
+        team: 'Real Madrid',
+        nationality: 'English',
+        jerseyNumber: 5,
+        age: 20,
+        imageUrl: './Images/bellingham.jpeg',
+    },
+    {
+        name: 'Ruben Dias',
+        team: 'Manchester City',
+        nationality: 'Portugeese',
+        jerseyNumber: 3,
+        age: 25,
+        imageUrl: './Images/dias.jpeg',
+    },
+    {
+        name: 'Paulo Dybala',
+        team: 'Roma',
+        nationality: 'Italian',
+        jerseyNumber: 10,
+        age: 28,
+        imageUrl: './Images/dybala.jpeg',
+    },
+    {
+        name: 'Phil Foden',
+        team: 'Manchester City',
+        nationality: 'English',
+        jerseyNumber: 47,
+        age: 23,
+        imageUrl: './Images/phil foden.jpg',
+    },
+    {
+        name: 'Kylian Mbappe',
+        team: 'Paris Saint-Germain',
+        nationality: 'French',
+        jerseyNumber: 10,
+        age: 25,
+        imageUrl: './Images/kylian.jpeg',
+    },
+    {
+        name: 'Paulo Maldinin',
+        team: 'Inter',
+        nationality: 'Italian',
+        jerseyNumber: 3,
+        age: 57,
+        imageUrl: '../Images/maldini.jpeg',
+    },
+
+];
+
+export default players;
